8484fe1836addffa3042097784685bd209cd5085
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-01-25 11:25:52 -0500

    Initial Commit

---
+++ TomaAgua/TomaAguaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomaAguaApp.swift
+//  TomaAgua
+//
+//  Created by Gabriel Marquez on 2023-01-25.
+//
+
+import SwiftUI
+
+@main
+struct TomaAguaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
